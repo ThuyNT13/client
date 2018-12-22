89af1c9b9438dbbaaa66ef966d57383d4a543b1c
ThuyNT13 <ngo.thuy38@gmail.com>
2018-12-22 14:36:15 -0800

    Initialize client auth, include scope to gapi

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,7 +3,12 @@ import { Component } from 'react';
 
 class GoogleAuth extends Component {
   componentDidMount() {
-    window.gapi.load('client:auth2');
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '629288277580-e6op6bl9i1lkgo3b8kojsjcpanhcs6hq.apps.googleusercontent.com',
+        scope: 'email'
+      })
+    });
   }
 
   render() {
