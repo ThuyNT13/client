0dc99875c24f3499a961d207bea3d34f9e67fa08
ThuyNT13 <ngo.thuy38@gmail.com>
2018-12-22 21:12:33 -0800

    Add state and set signedIn state to GoogleAuth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,18 +2,33 @@ import React from 'react';
 import { Component } from 'react';
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '629288277580-e6op6bl9i1lkgo3b8kojsjcpanhcs6hq.apps.googleusercontent.com',
         scope: 'email'
-      })
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+      });
     });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Unknown login state.</div>
+    } else if (this.state.isSignedIn) {
+      return <div>Signed in.</div>
+    } else {
+      return <div>Signed out.</div>
+    }
   }
 
   render() {
     return (
-      <div>GoogleAuth</div>
+      <div>{this.renderAuthButton()}</div>
     )
   }
 }
