99030c8d7cb9e96486eea949aa94d424ca7cd1dc
ThuyNT13 <ngo.thuy38@gmail.com>
2018-12-28 10:51:59 -0800

    Render and connect fetchStreams reducer to StreamList component

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return (
-    <div>
-      StreamList
-    </div>
-  )
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <article className="item" key={stream.id}>
+          <i className="large middle aligned icon camera"/>
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </article>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <section>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </section>
+    )
+  }
 };
 
-export default StreamList;
+const mapStateToProps = (state) => {
+  // return array of object's key-value pairs
+  return { streams: Object.values(state.streams) }
+};
+
+export default connect(mapStateToProps, { fetchStreams: fetchStreams })(StreamList);
