32944fa943467d5406faad7b4d4a9ee565af4e76
ThuyNT13 <ngo.thuy38@gmail.com>
2018-12-27 23:48:42 -0800

    Implement show, edit and delete streams action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,13 @@
+import streams from '../apis/streams';
 import {
   SIGN_IN,
   SIGN_OUT,
-  CREATE_STREAM
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
 } from './types';
-
-import streams from '../apis/streams';
 
 export const signIn = (userId) => {
   return {
@@ -16,6 +19,28 @@ export const signIn = (userId) => {
 export const signOut = () => {
   return {
     type: SIGN_OUT
+  };
+};
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get('/streams');
+
+    dispatch({
+      type: FETCH_STREAMS,
+      payload: response.data
+    });
+  };
+};
+
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({
+      type: FETCH_STREAM,
+      payload: response.data
+    });
   };
 };
 
@@ -31,6 +56,24 @@ export const createStream = (formValues) => {
   };
 };
 
-// export const createStream = (formValues) => async (dispatch) => {
-//   streams.post('/streams', formValues);
-// };
+export const editStream = (id, formValues) => {
+  return async (dispatch) => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({
+      type: EDIT_STREAM,
+      payload: response.data
+    });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({
+      type: DELETE_STREAM,
+      payload: id
+    });
+  };
+};
